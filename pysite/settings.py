1a7ccaf883a86c7acbd6f29ec84b6fdccfc94fdc
rafael1717y <rafaelufjf@gmail.com>
2020-09-03 12:04:10 -0300

    close #25

--- pysite/settings.py
+++ pysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k%_)=wch+iw40ww!mi7j72!fz$nl!8(7!fx7k^55q$!6rcna!3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
