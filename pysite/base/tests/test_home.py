0156c293eab94cef3438fc709d698ea99b62c035
rafael1717y <rafaelufjf@gmail.com>
2020-09-08 10:42:50 -0300

    Landing Page

--- pysite/base/tests/test_home.py
+++ pysite/base/tests/test_home.py
@@ -1,6 +1,11 @@
 from django.test import Client
+from pysite.django_assertions import assert_contains
 
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Site do Rafa</title>')
