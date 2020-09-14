5e214a4895c86e5263758001a6e967441e7aeaa8
rafael1717y <rafaelufjf@gmail.com>
2020-09-14 15:22:23 -0300

    inclusão de vídeo do vimeo

--- pysite/aperitivos/tests.py
+++ pysite/aperitivos/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import pytest
 from django.urls import reverse
+from pysite.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
@@ -8,3 +9,12 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code==200
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>'
+
+'''## colocar o video aqui
+def test_conteudo_video(resp):
+     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/251224475"'>'
+'''
+
