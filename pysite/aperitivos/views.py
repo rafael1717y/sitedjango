891f27b606832f9f1912a8313761f2d772445799
rafael1717y <rafaelufjf@gmail.com>
2020-09-15 14:00:48 -0300

    Incorporado primeiro vídeo do Vimeo de forma manual.

--- pysite/aperitivos/views.py
+++ pysite/aperitivos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo':'Vídeo Aperitivo: Motivação', 'vimeo_id': 458138324}
+    return render(request, 'aperitivos/video.html', context={'video':video})
